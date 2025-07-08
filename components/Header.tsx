ae54cf3c4ae7b1d135512c5f7c8d1f4b2ec3162a
Simon Schödler <simon.schoedler@hotmail.ch>
2025-07-09 00:32:20 +0200

    Restyles header, adds ®'s for brandnames

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,9 @@
 export function Header() {
   return (
-    <div className="flex flex-col w-full justify-between mb-8">
-      <h1 class="text-6xl mb-2"><i>the</i> nutrition-planner</h1>
-      <h2 class="text-2xl">and this is a subtitle</h2>
+    <div className="flex flex-col w-full justify-between mb-16">
+      <h1 class="text-6xl mb-2">
+        <i class="font-normal font-body text-base">the</i> nutrition-planner
+      </h1>
     </div>
   );
 }
